643c2fd518d8783e972721dfd07419b13b96b7af
shadowfalls <dinesh.muralidaran@gmail.com>
2020-08-16 21:24:44 +0530

    2rd deployment

---
+++ precache-manifest.f56a051a2447c49502cfe08b1ae6d43d.js
@@ -0,0 +1,64 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342136aa492bf25ceaf9616055a60866",
+    "url": "./index.html"
+  },
+  {
+    "url": "1.2c8ea76b7aa6b4bc8e24.css"
+  },
+  {
+    "revision": "40c7a3c4b339ee2bf8268dec663c2d68",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "icons/android/icon_144x144.6d1fa62694844088b94d51170b52ac55.png"
+  },
+  {
+    "url": "icons/android/icon_192x192.2f64f67f6a49fa91fa8ce3442a16bed8.png"
+  },
+  {
+    "url": "icons/android/icon_36x36.685c51b5cce074397182df276cf6a931.png"
+  },
+  {
+    "url": "icons/android/icon_48x48.4233c0d5b952f8d75c8d38c140b6a76d.png"
+  },
+  {
+    "url": "icons/android/icon_512x512.955663924328f335f6a66c9193e52be5.png"
+  },
+  {
+    "url": "icons/android/icon_72x72.ca445f10c67802f3d7f636a3db0854bc.png"
+  },
+  {
+    "url": "icons/android/icon_96x96.554ea64baa5259715f9c90355be10fa2.png"
+  },
+  {
+    "url": "icons/ios/icon_128x128.cfc1d889bdda7493843ac29a266c71ac.png"
+  },
+  {
+    "url": "icons/ios/icon_192x192.2f64f67f6a49fa91fa8ce3442a16bed8.png"
+  },
+  {
+    "url": "icons/ios/icon_256x256.d71c3a21f190a83737eed3f8bd6b2c7a.png"
+  },
+  {
+    "url": "icons/ios/icon_384x384.c323a8e45a7c3434e6833a8163b30d8a.png"
+  },
+  {
+    "url": "icons/ios/icon_512x512.955663924328f335f6a66c9193e52be5.png"
+  },
+  {
+    "url": "icons/ios/icon_96x96.554ea64baa5259715f9c90355be10fa2.png"
+  },
+  {
+    "url": "main.2c8ea76b7aa6b4bc8e24.bundle.js"
+  },
+  {
+    "url": "main.2c8ea76b7aa6b4bc8e24.css"
+  },
+  {
+    "url": "manifest.97c3ba972efd364330224e6ea8772ce7.json"
+  },
+  {
+    "url": "vendors~main.2c8ea76b7aa6b4bc8e24.bundle.js"
+  }
+]);
